331344a1ee8776f1a66a3effb353fc98705fa8b0
Craig Johnson <craigjart@gmail.com>
2024-07-23 05:01:44 -0400

    Stash

--- entry_lich.c
+++ entry_lich.c
@@ -1,9 +1,15 @@
 #define MAX_ENTITY_COUNT 1024
 
 typedef enum EntityTypes{
-    EN_null   = 0,
+    EN_nil    = 0,
     EN_player = 1
 }EntityTypes;
+
+typedef enum SpriteID {
+    SP_nil     = 0,
+    SP_player  = 1,
+    SP_MAX     = 2
+}SpriteID;
 
 typedef struct {
     bool is_valid;
@@ -13,20 +19,18 @@ typedef struct {
     Vector2 pos;
 
     bool render_sprite;
-    int sprite_size;
-    Gfx_Image* sprite_sheet;
+    SpriteID sprite_id;
 }Entity;
+
+typedef struct Sprite {
+    Gfx_Image* sprite_sheet;
+}Sprite;
+Sprite sprites[SP_MAX];
 
 typedef struct {
     Entity entities[MAX_ENTITY_COUNT];
 }World;
 World *world = 0;
-
-typedef enum SpriteID {
-    SP_null,
-    SP_player,
-    SP_MAX
-}SpriteID;
 
 Entity* create_entity() {
     Entity* entity_found = 0;
@@ -42,7 +46,11 @@ Entity* create_entity() {
 }
 
 void free_entity() {
+}
 
+Sprite* get_sprite(int sprite_id) {
+    if (sprite_id == 0 || sprite_id == SP_MAX) { return &sprites[0]; }
+    return &sprites[sprite_id];
 }
 
 int entry(int argc, char **argv) {
@@ -53,16 +61,18 @@ int entry(int argc, char **argv) {
 	window.y = 200;
 	window.clear_color = hex_to_rgba(0xbebebeff);
 
-    float64 seconds_counter = 0.0;
     float64 second_counter = 0.0;
     s32 frame_count = 0;
     float64 last_time = os_get_current_time_in_seconds();
     float64 last_fixed_time = os_get_current_time_in_seconds();
 
     world = alloc(get_heap_allocator(), sizeof(World));
+    // sprites[SP_player];
+
+    sprites[SP_player] = (Sprite) { .sprite_sheet=load_image_from_disk(fixed_string("assets/character.png"), get_heap_allocator()) };
 
     Entity* player_en = create_entity();
-    player_en->sprite_sheet = load_image_from_disk(fixed_string("assets/character.png"), get_heap_allocator());
+    player_en->sprite_id = SP_player;
     player_en->pos = v2(0,0);
     player_en->scale = v2(16.0,16.0);
 
@@ -87,19 +97,18 @@ int entry(int argc, char **argv) {
 
         for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
             Entity* entity = &world->entities[i];
+            Sprite* sprite = get_sprite(entity->sprite_id);
             Matrix4 xform = m4_scalar(1.0);
             xform = m4_translate(xform, v3(entity->pos.x, entity->pos.y, 0));
-            Draw_Quad* quad = draw_image_xform(entity->sprite_sheet, xform, entity->scale, COLOR_WHITE);
+            Draw_Quad* quad = draw_image_xform(sprite->sprite_sheet, xform, entity->scale, COLOR_WHITE);
         }
 
 		gfx_update();
 
-        seconds_counter += delta_t;
         second_counter += delta_t;
         frame_count += 1;
         if (second_counter > 1.0) {
             seconds_counter = roundf(seconds_counter);
-            log("Seconds played: %f", seconds_counter);
             log("Fps: %i", frame_count);
             second_counter = 0.0;
             frame_count = 0.0;
